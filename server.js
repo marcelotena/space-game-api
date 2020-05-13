cd06453cca5fb81cc0740b8668b578c1d5073364
Marcelo Tena <marcelotena@fastandyours.com>
2020-05-13 13:35:01 +0200

    Database and api routes config

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Route files
+const auth = require('./routes/api/auth');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+// Mount routers
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/profile', profile);
+app.use('/api/v1/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`.cyan));
