498ba26cca95e6ca3451fdf539ba89a9d1d70c41
Marcelo Tena <marcelotena@fastandyours.com>
2020-05-13 18:45:55 +0200

    Log-in, log-out (delete cookies), get current user and update user details.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -15,10 +15,10 @@ const router = express.Router();
 const { protect } = require('../../middleware/auth');
 
 router.post('/register', register);
-// router.post('/login', login);
-// router.get('/logout', logout);
-// router.get('/me', protect, getMe);
-// router.put('/updatedetails', protect, updateDetails);
+router.post('/login', login);
+router.get('/logout', logout);
+router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
 // router.put('/updatepassword', protect, updatePassword);
 // router.post('/forgotpassword', forgotPassword);
 // router.put('/resetpassword/:resettoken', resetPassword);
