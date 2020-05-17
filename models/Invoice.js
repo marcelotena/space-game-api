0aad09dbb427aacde27a4a4e625636e7888476e9
Marcelo Tena <marcelotena@fastandyours.com>
2020-05-17 11:41:02 +0200

    Added to Invoice Model: totalTax, totalRetention and currency.

--- models/Invoice.js
+++ models/Invoice.js
@@ -48,6 +48,11 @@ const InvoiceSchema = new mongoose.Schema({
       base: Number
     }
   ],
+  currency: {
+    type: String,
+    default: 'EUR',
+    enum: ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BOV", "BRL", "BSD", "BTN", "BWP", "BYR", "BZD", "CAD", "CDF", "CHE", "CHF", "CHW", "CLF", "CLP", "CNY", "COP", "COU", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MXV", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STD", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "USN", "USS", "UYI", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XBA", "XBB", "XBC", "XBD", "XCD", "XDR", "XFU", "XOF", "XPD", "XPF", "XPT", "XTS", "XXX", "YER", "ZAR", "ZMW"]
+  },
   baseAmount: {
     type: Number,
     default: function() {
@@ -63,9 +68,31 @@ const InvoiceSchema = new mongoose.Schema({
     type: Number,
     default: 21
   },
+  totalTax: {
+    type: Number,
+    default: function() {
+      if (this.items) {
+        return this.items.reduce((prev, cur) => {
+          return prev + cur.base * cur.quantity * this.invoiceTax / 100;
+        }, 0);
+      }
+      return 0;
+    }
+  },
   invoiceRetention: {
     type: Number,
     default: 0
+  },
+  totalRetention: {
+    type: Number,
+    default: function() {
+      if (this.items) {
+        return this.items.reduce((prev, cur) => {
+          return prev + cur.base * cur.quantity * (-this.invoiceRetention / 100);
+        }, 0);
+      }
+      return 0;
+    }
   },
   invoiceDate: {
     type: Date,
